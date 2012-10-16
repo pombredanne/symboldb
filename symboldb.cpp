c25e774c49444bef5e41895071f15f6c51df4acc
Florian Weimer <fweimer@redhat.com>
2012-10-16 13:40:34 +0200

    Print usage if parameter count is wrong

--- symboldb.cpp
+++ symboldb.cpp
@@ -31,6 +31,11 @@ static find_symbols_callbacks fsc = {dump_def, dump_ref};
 int
 main(int argc, char **argv)
 {
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s ELF-FILE\n", argv[0]);
+    exit(1);
+  }
+
   elf_version(EV_CURRENT);
 
   int fd = ::open(argv[1], O_RDONLY);
