2dc3f2c57c916391c1992b05e6072de1b12df41f
Florian Weimer <fweimer@redhat.com>
2012-12-21 13:56:54 +0100

    Include licensing information

--- soname-dependency-closure.py
+++ soname-dependency-closure.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+# Copyright (C) 2012 Red Hat, Inc.
+# Written by Florian Weimer <fweimer@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # This is a demo implementation of the DT_NEEDED resolver because the
 # initial attempt in SQL resulted in far too much data.  This Python
