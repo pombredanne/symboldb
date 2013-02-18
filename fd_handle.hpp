257f70d81b74a2615c2b637e0f639542f58baba3
Florian Weimer <fweimer@redhat.com>
2013-02-18 15:40:37 +0100

    Add class fd_handle

---
+++ fd_handle.hpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2013 Red Hat, Inc.
+ * Written by Florian Weimer <fweimer@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// POSIX file descriptor which is closed on scope exit.
+struct fd_handle {
+  int raw;
+
+  fd_handle()
+    : raw(-1)
+  {
+  }
+
+  // Closes RAW if it is not negative.
+  ~fd_handle();
+};
