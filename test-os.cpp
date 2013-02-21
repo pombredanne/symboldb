91df369578b264e40ad63ffd6d917f017697ecaa
Florian Weimer <fweimer@redhat.com>
2013-02-21 13:49:29 +0100

    Fix dates in copyright notices

--- test-os.cpp
+++ test-os.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012, 2013 Red Hat, Inc.
+ * Copyright (C) 2013 Red Hat, Inc.
  * Written by Florian Weimer <fweimer@redhat.com>
  *
  * This program is free software: you can redistribute it and/or modify
