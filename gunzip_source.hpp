5af7f095c2b2784d3fa7db24b531e510caeb3677
Florian Weimer <fweimer@redhat.com>
2013-02-23 14:33:59 +0100

    Add gunzip_source and zlib_inflate_exception

---
+++ gunzip_source.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 Red Hat, Inc.
+ * Written by Florian Weimer <fweimer@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "source.hpp"
+
+#include <tr1/memory>
+
+// Decompresses the source using the gzip algorithm.  Does not take
+// ownership of the pointer.
+class gunzip_source : public source {
+  struct impl;
+  std::tr1::shared_ptr<impl> impl_;
+public:
+  gunzip_source(source *);
+  ~gunzip_source();
+
+  size_t read(unsigned char *, size_t);
+};
