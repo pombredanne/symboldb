0276007b9713ee5ff59ff9a33da478ef3754d46f
Florian Weimer <fweimer@redhat.com>
2013-03-19 08:38:29 +0100

    pg_query: Extend to 12 parameters

--- test/test-pg_testdb.cpp
+++ test/test-pg_testdb.cpp
@@ -163,6 +163,16 @@ test()
 	     1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     CHECK(r.ntuples() == 1);
     COMPARE_STRING(r.getvalue(0, 0), "{1,2,3,4,5,6,7,8,9,10}");
+    pg_query
+      (h, r, "SELECT ARRAY[$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11]",
+       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+    CHECK(r.ntuples() == 1);
+    COMPARE_STRING(r.getvalue(0, 0), "{1,2,3,4,5,6,7,8,9,10,11}");
+    pg_query
+      (h, r, "SELECT ARRAY[$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12]",
+       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+    CHECK(r.ntuples() == 1);
+    COMPARE_STRING(r.getvalue(0, 0), "{1,2,3,4,5,6,7,8,9,10,11,12}");
 
     pg_query_binary(h, r, "SELECT ARRAY[$1]::text", 1);
     CHECK(r.ntuples() == 1);
@@ -207,6 +217,18 @@ test()
 		    1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     CHECK(r.ntuples() == 1);
     COMPARE_STRING(r.getvalue(0, 0), "{1,2,3,4,5,6,7,8,9,10}");
+    pg_query_binary(h, r,
+		    "SELECT ARRAY[$1, $2, $3, $4, $5, $6, $7, $8, $9, $10,"
+		    " $11]::text",
+		    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+    CHECK(r.ntuples() == 1);
+    COMPARE_STRING(r.getvalue(0, 0), "{1,2,3,4,5,6,7,8,9,10,11}");
+    pg_query_binary(h, r,
+		    "SELECT ARRAY[$1, $2, $3, $4, $5, $6, $7, $8, $9, $10,"
+		    " $11, $12]::text",
+		    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+    CHECK(r.ntuples() == 1);
+    COMPARE_STRING(r.getvalue(0, 0), "{1,2,3,4,5,6,7,8,9,10,11,12}");
 
     ////////////////////////////////////////////////////////////////////
     // Response decoding
