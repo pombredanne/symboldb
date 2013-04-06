7835e1e0d01c4a413389db0c9a9751a1c3588ec2
Florian Weimer <fweimer@redhat.com>
2013-04-06 18:04:47 +0200

    cxxll::file_cache: Reindent test case

--- test/test-file_cache.cpp
+++ test/test-file_cache.cpp
@@ -35,61 +35,59 @@ test()
   temporary_directory tempdir
     ((temporary_directory_path() + "/test-file_cache-").c_str());
 
-  {
-    file_cache fc(tempdir.path().c_str());
-    static const char valid[] = "valid";
-    std::vector<unsigned char> data(valid, valid + sizeof(valid));
-    checksum csum;
-    csum.type = hash_sink::sha256;
-    static const unsigned char csum_value[32] = {
-      0x54, 0x3a, 0xfb, 0x82, 0xad, 0x21, 0xc0, 0x2e, 0x05, 0xde, 0xef,
-      0x9b, 0xc5, 0x53, 0x90, 0x4b, 0x2b, 0xb6, 0xae, 0x10, 0xbe, 0x33,
-      0x7d, 0x0b, 0x7c, 0xd6, 0xe1, 0x50, 0x99, 0xd1, 0x1b, 0xcf
-    };
-    csum.value.insert(csum.value.end(),
-		      csum_value, csum_value + sizeof(csum_value));
-    csum.length = sizeof(valid);
-    std::string path;
+  file_cache fc(tempdir.path().c_str());
+  static const char valid[] = "valid";
+  std::vector<unsigned char> data(valid, valid + sizeof(valid));
+  checksum csum;
+  csum.type = hash_sink::sha256;
+  static const unsigned char csum_value[32] = {
+    0x54, 0x3a, 0xfb, 0x82, 0xad, 0x21, 0xc0, 0x2e, 0x05, 0xde, 0xef,
+    0x9b, 0xc5, 0x53, 0x90, 0x4b, 0x2b, 0xb6, 0xae, 0x10, 0xbe, 0x33,
+    0x7d, 0x0b, 0x7c, 0xd6, 0xe1, 0x50, 0x99, 0xd1, 0x1b, 0xcf
+  };
+  csum.value.insert(csum.value.end(),
+		    csum_value, csum_value + sizeof(csum_value));
+  csum.length = sizeof(valid);
+  std::string path;
+  fc.add(csum, data, path);
+  COMPARE_STRING(path,
+		 tempdir.path(base16_encode(csum.value.begin(),
+					    csum.value.end()).c_str()));
+  CHECK(access(path.c_str(), R_OK) == 0);
+  std::vector<std::vector<unsigned char> > digests;
+  fc.digests(digests);
+  CHECK(digests.size() == 1);
+  COMPARE_STRING(base16_encode(digests.front().begin(),
+			       digests.front().end()),
+		 base16_encode(csum.value.begin(), csum.value.end()));
+  CHECK(access((path + ".tmp").c_str(), R_OK) == -1 && errno == ENOENT);
+  CHECK(unlink(path.c_str()) == 0);
+
+  csum.length = 0;
+  std::string old_path;
+  std::swap(path, old_path);
+  path = "abc";
+  try {
     fc.add(csum, data, path);
-    COMPARE_STRING(path,
-		   tempdir.path(base16_encode(csum.value.begin(),
-					      csum.value.end()).c_str()));
-    CHECK(access(path.c_str(), R_OK) == 0);
-    std::vector<std::vector<unsigned char> > digests;
-    fc.digests(digests);
-    CHECK(digests.size() == 1);
-    COMPARE_STRING(base16_encode(digests.front().begin(),
-				 digests.front().end()),
-		   base16_encode(csum.value.begin(), csum.value.end()));
-    CHECK(access((path + ".tmp").c_str(), R_OK) == -1 && errno == ENOENT);
-    CHECK(unlink(path.c_str()) == 0);
-
-    csum.length = 0;
-    std::string old_path;
-    std::swap(path, old_path);
-    path = "abc";
-    try {
-      fc.add(csum, data, path);
-      CHECK(0 && "missing exception");
-    } catch (file_cache::checksum_mismatch &e) {
-      COMPARE_STRING(e.what(), "length");
-    }
-    COMPARE_STRING(path, "abc");
-    CHECK(access(old_path.c_str(), R_OK) == -1 && errno == ENOENT);
-    CHECK(access((old_path + ".tmp").c_str(), R_OK) == -1 && errno == ENOENT);
-
-    ++csum.value.front();
-    csum.length = sizeof(valid);
-    try {
-      fc.add(csum, data, path);
-      CHECK(0 && "missing exception");
-    } catch (file_cache::checksum_mismatch &e) {
-      COMPARE_STRING(e.what(), "digest");
-    }
-    COMPARE_STRING(path, "abc");
-    CHECK(access(old_path.c_str(), R_OK) == -1 && errno == ENOENT);
-    CHECK(access((old_path + ".tmp").c_str(), R_OK) == -1 && errno == ENOENT);
+    CHECK(0 && "missing exception");
+  } catch (file_cache::checksum_mismatch &e) {
+    COMPARE_STRING(e.what(), "length");
   }
+  COMPARE_STRING(path, "abc");
+  CHECK(access(old_path.c_str(), R_OK) == -1 && errno == ENOENT);
+  CHECK(access((old_path + ".tmp").c_str(), R_OK) == -1 && errno == ENOENT);
+
+  ++csum.value.front();
+  csum.length = sizeof(valid);
+  try {
+    fc.add(csum, data, path);
+    CHECK(0 && "missing exception");
+  } catch (file_cache::checksum_mismatch &e) {
+    COMPARE_STRING(e.what(), "digest");
+  }
+  COMPARE_STRING(path, "abc");
+  CHECK(access(old_path.c_str(), R_OK) == -1 && errno == ENOENT);
+  CHECK(access((old_path + ".tmp").c_str(), R_OK) == -1 && errno == ENOENT);
 }
 
 static test_register t("file_cache", test);
