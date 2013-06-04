0b6909a3c85f56b033f387d958a48fd6860b203a
Florian Weimer <fweimer@redhat.com>
2013-06-04 16:23:14 +0200

    cxxll::rpmfi_handle: Add class

---
+++ include/cxxll/rpmfi_handle.hpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2013 Red Hat, Inc.
+ * Written by Florian Weimer <fweimer@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <rpm/rpmfi.h>
+
+namespace cxxll {
+
+// Minimal wrapper around rpmfi pointers.
+class rpmfi_handle {
+  rpmfi raw_;
+  rpmfi_handle(const rpmfi_handle &); // not implemented
+  rpmfi_handle &operator=(const rpmfi_handle &); // not implemented
+public:
+  // Throws rpm_parser_exception on failure.
+  explicit rpmfi_handle(Header);
+  ~rpmfi_handle();
+
+  // Returns the raw rpmfi handle.
+  rpmfi get();
+
+  // Invokes rpmfiNext() and returns true if there was another file
+  // element.
+  bool next();
+};
+
+inline rpmfi
+rpmfi_handle::get()
+{
+  return raw_;
+}
+
+inline bool
+rpmfi_handle::next()
+{
+  return rpmfiNext(raw_) != -1;
+}
+
+} // namespace cxxll
---
+++ lib/cxxll/rpmfi_handle.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2013 Red Hat, Inc.
+ * Written by Florian Weimer <fweimer@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cxxll/rpmfi_handle.hpp>
+#include <cxxll/rpm_parser_exception.hpp>
+
+cxxll::rpmfi_handle::rpmfi_handle(Header header)
+{
+  raw_ = rpmfiNew(0, header, 0, 0);
+  if (raw_ == NULL) {
+    throw rpm_parser_exception("rpmfiNew failed");
+  }
+}
+
+cxxll::rpmfi_handle::~rpmfi_handle()
+{
+  rpmfiFree(raw_);
+}
